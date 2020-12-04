7796bfd1f571fcf6aea371010829395c8ac04731
GQiangQ <740407980@qq.com>
2020-12-05 00:55:11 +0800

    完成翻译

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,60 @@ import * as  https from 'https' ;
 import * as querystring from 'querystring'
 import * as md5 from 'md5'
 
-export const translate = (word)=>{
+import {appid, appSecret } from '../private_data'
+import { type } from 'os';
+
+export const translate = (q:string)=>{
+  let from = 'en'
+  let to = 'zh'
+  if(!/[a-zA-Z]/.test(q[0])){
+    from = 'zh'
+    to = 'en'
+  }
+  let salt:number = Math.random()
+  const sign = md5(appid+q+salt+appSecret)
   const query:string  = querystring.stringify({
-    q:word,
-    from: 'en',
-    to: 'zh',
-    appid: '',
-    salt:Math.random(),
-    sing: md5('123'),
+    q,
+    from,
+    to,
+    appid,
+    salt, // 随机数
+    sign, // 密钥
   })
   const options = {
     hostname: 'fanyi-api.baidu.com',
     port: 443,
-    path: '/api/trans/vip/translate',
+    path: '/api/trans/vip/translate?' + query,
     method: 'GET'
   };
-  const req = https.request(options, (res) => {
-    console.log('状态码:', res.statusCode);
-    console.log('请求头:', res.headers);
-    res.on('data', (d) => {
-      process.stdout.write(d);
+  const request = https.request(options, (response) => {
+    const chunks = []
+    response.on('data', (chunk) => {
+      chunks.push(chunk)
+    });
+    response.on('end', () => {
+      type BaiduResult = {
+        from:string;
+        to:string;
+        trans_result:{
+          src:string;
+          dst:string;
+        }[];
+        error_code?:string;
+        error_msg?:string;
+      }
+      const data:BaiduResult = JSON.parse(Buffer.concat(chunks).toString())
+      if (data.error_code){
+        console.log(data.error_code+':','太难了我翻译不出来呀！！！')
+        process.exit(2)
+      }else{
+        console.log(data.trans_result[0])
+        process.exit(0)
+      }
     });
   });
-  req.on('error', (e) => {
+  request.on('error', (e) => {
     console.error(e);
   });
-  req.end();
+  request.end();
 }
